a1cc8dbd7241ac69db510448e5c1b95285b42184
Carola Jenke <carola.jenke@iste.uni-stuttgart.de>
2021-09-06 10:34:53 +0200

    new example queries JavaScript and Python

--- src/app/examples.ts
+++ src/app/examples.ts
@@ -21,19 +21,33 @@ export const EXAMPLES_JAVA: Example[] = [
 
 // Only for testing the functionality
 export const EXAMPLES_JAVASCRIPT: Example[] = [
-  { query: 'JavaScript 1', old: '_', new: 'ID.ID = <...>; _'},
-  { query: 'JavaScript 2', old: '_', new: 'ID.ID = LT;'},
-  { query: 'JavaScript 3', old: 'ID<0>: LT', new: 'ID<0>: LT'},
-  { query: 'JavaScript 4', old: 'this.ID<0>(EXPR);', new: 'this.ID<0>(EXPR);'},
-  { query: 'JavaScript 5', old: 'if (ID) { ID.ID(<...>);', new: '<...>'},
-  { query: 'JavaScript 6', old: '<...> = EXPR<0>;', new: '<...> = EXPR<0> binOP EXPR;'},
-  { query: 'JavaScript 7', old: 'return EXPR;', new: '_'},
-  { query: 'JavaScript 8', old: 'ID.ID(<...>);', new: '_'},
-  { query: 'JavaScript 9', old: 'ID();', new: '_'},
-  { query: 'JavaScript 10', old: 'ID<0>.ID<1>(<...>);', new: 'ID<2>.ID<1>(<...>);'},
+  { query: 'JavaScript 1', old: 'if (ID<0>) { <...>', new: 'if (ID<0> && EXPR) { <...>'},
+  { query: 'JavaScript 2', old: 'if (ID<0>) { <...>', new: 'if (ID<0> || EXPR) { <...>'},
+  { query: 'JavaScript 3', old: 'EXPR.ID<0>();', new: 'EXPR.ID<0>();'},
+  { query: 'JavaScript 4', old: 'return ID;', new: 'return null;'},
+  { query: 'JavaScript 5', old: 'EXPR<0>.ID()', new: 'EXPR<0>.ID()'},
+  { query: 'JavaScript 6', old: 'ID<0>(ID);', new: 'ID<0>();'},
+  { query: 'JavaScript 7', old: 'this.ID<0> = ID.ID(ID<0>);', new: 'this.ID<0> = ID<0>;'},
+  { query: 'JavaScript 8', old: 'ID<0> = EXPR;', new: 'ID<0> = EXPR;'},
+  { query: 'JavaScript 9', old: 'ID(EXPR); ID(EXPR);', new: 'ID(EXPR); ID(EXPR);'},
+  { query: 'JavaScript 10', old: '<...>', new: 'if (<...>) { throw EXPR'},
+  { query: 'JavaScript 11', old: 'throw EXPR', new: '_'},
 
 
 ];
 
 // Only for testing the functionality
-export const EXAMPLES_PYTHON: Example[] = [];
+export const EXAMPLES_PYTHON: Example[] = [
+  { query: 'Python 1', old:'if ID<0>: <...>', new: 'if ID<0> and EXPR: <...>'},
+  { query: 'Python 2', old:'if ID<0>: <...>', new: 'if ID<0> or EXPR: <...>'},
+  { query: 'Python 3', old:'EXPR.ID<0>()', new: 'EXPR.ID<0>()'},
+  { query: 'Python 4', old:'ID<0>.ID(<...>)', new: 'ID<0>.ID(<...>)'},
+  { query: 'Python 5', old:'return ID', new: 'return None'},
+  { query: 'Python 6', old:'EXPR<0>.ID()', new: 'EXPR<0>.ID()'},
+  { query: 'Python 7', old:'ID.ID.ID()', new: 'ID.ID.ID()'},
+  { query: 'Python 8', old:'ID = ID<0>.ID(ID)', new: 'ID<0>.ID(ID)'},
+  { query: 'Python 9', old:'ID<0>(EXPR)', new: 'ID<0>(EXPR)'},
+  { query: 'Python 10', old:'while(EXPR): <...>', new: 'while(EXPR): <...>'},
+  { query: 'Python 11', old:'print(LT)', new: 'print(LT)'}
+];
+
